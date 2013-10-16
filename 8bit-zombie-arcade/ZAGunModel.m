f4326c1f1a52ed89647afa3f90c9734d90675998
Christian Hansen <chansencc@gmail.com>
2013-10-16 15:23:53 -0700

    Particle tests

--- 8bit-zombie-arcade/ZAGunModel.m
+++ 8bit-zombie-arcade/ZAGunModel.m
@@ -10,6 +10,27 @@
 
 @implementation ZAGunModel
 
+//- (SKEmitterNode *)bulletEmitterNodeWithSpeed:(float)speed lifetime:(float)lifetime scale:(float)scale birthRate:(float)birthRate color:(SKColor*)color
+//{
+//    SKLabelNode *star =
+//    [SKLabelNode labelNodeWithFontNamed:@"Helvetica"];
+//    star.fontSize = 80.0f; star.text = @"+";
+//    SKTexture *texture;
+//    SKView *textureView = [SKView new];
+//    texture = [textureView textureFromNode:star]; texture.filteringMode = SKTextureFilteringNearest;
+//}
 
+
+- (SKEmitterNode *)bulletEmitterNode
+{
+    NSString *bulletPath = [[NSBundle mainBundle] pathForResource:@"BulletParticle" ofType:@"sks"];
+    SKEmitterNode *bulletParticle = [NSKeyedUnarchiver unarchiveObjectWithFile:bulletPath];
+    bulletParticle.particlePosition = CGPointMake(0, 0);
+    bulletParticle.particleBirthRate = 5;
+    return bulletParticle;
+    
+    [self addChild:bulletParticle];
+    
+}
 
 @end
