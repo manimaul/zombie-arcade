9a3344a8c7f997d0d989fed40340a80cd97b9bc4
Christian Hansen <chansencc@gmail.com>
2013-10-17 13:27:57 -0700

    Commit so I can pull from master.

--- 8bit-zombie-arcade/ZAZombieSpriteNode.m
+++ 8bit-zombie-arcade/ZAZombieSpriteNode.m
@@ -7,6 +7,8 @@
 //
 
 #import "ZAZombieSpriteNode.h"
+#import "ZAEntity.h"
+#import "ZAGunModel.h"
 
 @implementation ZAZombieSpriteNode
 
@@ -14,6 +16,26 @@
 {
     ZAZombieSpriteNode *zombieSprite = [[ZAZombieSpriteNode alloc] initWithCharachterType:zombie];
     return zombieSprite;
+}
+
+#pragma mark Physics and Collision
+
+- (void)configureCollisionBody
+{
+    self.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.frame.size];
+    
+    self.physicsBody.affectedByGravity = NO;
+    
+    // Set the category of the physics object that will be used for collisions
+    self.physicsBody.categoryBitMask = ColliderTypeZombie;
+    
+    // We want to know when a collision happens but we dont want the bodies to actually react to each other so we
+    // set the collisionBitMask to 0
+    self.physicsBody.collisionBitMask = 0;
+    
+    // Make sure we get told about these collisions
+    self.physicsBody.contactTestBitMask = ColliderTypeHero | ColliderTypeBullet;
+    
 }
 
 @end
